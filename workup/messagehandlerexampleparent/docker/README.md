417045a1c226c89eaabbbf7eeee2f7e37d82ee67
gallenc <cg02r@zepler.net>
2020-10-26 16:01:12 +0000

    adding docker build commands to maven build

---
+++ workup/messagehandlerexampleparent/docker/README.md
@@ -0,0 +1,2 @@
+## build file for docker images
+## see https://stackify.com/guide-docker-java/
---
+++ workup/messagehandlerexampleparent/docker/buildDockerImage.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# A sample Bash script, to build docker file
+echo Building docker image
+docker build -t messagehandler:latest .
