7f366eaf50aa482a2181ca04d43f43cb06c57178
ruipinto17 <ruipintopsi@outlook.pt>
2020-10-26 15:17:23 +0000

    removed test project

--- mainProject2/parent/userinterface/model/src/main/java/org/solent/com504/project/model/user/service/SecurityService.java
+++
@@ -1,7 +0,0 @@
-package org.solent.com504.project.model.user.service;
-
-public interface SecurityService {
-    String findLoggedInUsername();
-
-    void autologin(String username, String password);
-}
